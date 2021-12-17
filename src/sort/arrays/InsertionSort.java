d04c39c76f0d6c216029e87604c3cb5a68d69220
SangVUDINH <43957998+SangVUDINH@users.noreply.github.com>
2021-12-17 19:04:22 +0100

    insertionsort recursive + mergesort DESC

--- src/sort/arrays/InsertionSort.java
+++ src/sort/arrays/InsertionSort.java
@@ -7,24 +7,50 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-		for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
-			int newElement = intArray[firstUnsortedIndex];
-			int i;
+		// ITERATIVE 
+//		for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+//			int newElement = intArray[firstUnsortedIndex];
+//			int i;
+//
+//			for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+//				intArray[i] = intArray[i - 1];
+////			on decale les chiffres superieur à newElement	
+//			}
+//			intArray[i] = newElement;
+//		}
+//
+//		System.out.println(Arrays.toString(intArray));
+//		
+//		
+//		System.out.println(10 % 3);
+//		int result = (int) Math.pow(10, 3);
+//		System.out.println(result);
+//		System.out.println(874725 / result % 10);
 
-			for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-				intArray[i] = intArray[i-1];
-//			on decale les chiffres superieur à newElement	
-			}
-			intArray[i] = newElement;
-		}
-
+		
+		// RECURSIVE
+		intersectionSort(intArray, intArray.length);
 		System.out.println(Arrays.toString(intArray));
-		System.out.println(10%3);
-		 int result = (int) Math.pow(10, 3);
-	     System.out.println(result);
-	     System.out.println(874725 / result %10);
-	     
-	     
 	}
+	
+	
+	public static void intersectionSort(int[] intArray,int numItems) {		
+		
+		if (numItems <2) {
+			return;
+		}
+		
+		intersectionSort(intArray,numItems-1);
+		
+		// chaque appel on trie le new element,
+		int newElement = intArray[numItems-1];
+		int i;
+		
+		for (i = numItems-1; i > 0 && intArray[i - 1] > newElement; i--) {
+			intArray[i] = intArray[i - 1];
+			//	on decale les chiffres superieur à newElement	
+		}
+		intArray[i] = newElement;
+		}
 
 }
--- src/sort/arrays/MergeSort.java
+++ src/sort/arrays/MergeSort.java
@@ -23,7 +23,7 @@ public class MergeSort {
 		
 		mergeSort(intArray, start, mid);
 		mergeSort(intArray, mid, end);		
-		merge(intArray,start,mid,end); // sort between 2 parts sorted
+		mergeDesc(intArray,start,mid,end); // sort between 2 parts sorted
 		
 	}
 	
@@ -43,6 +43,29 @@ public class MergeSort {
 		
 		while(i <mid && j<end) {
 			intTemp[tempIndex++] = intArray[i] <= intArray[j] ? intArray[i++] : intArray[j++];
+		}
+		
+		// optimisation sur la copy, copy par bloc... a voir en details
+		System.arraycopy(intArray, i, intArray, start+tempIndex, mid-i);
+		System.arraycopy(intTemp, 0, intArray, start, tempIndex);
+	}
+	
+	public static void mergeDesc(int[] intArray, int start,int mid, int end) {
+		
+		// 	pas besoin de trier si le dernier element du 1er part est inferieur 
+		//	à 1er element du 2eme part
+		if (intArray[mid -1] >= intArray[mid]) {
+			return;
+		}
+		
+		int i = start;
+		int j = mid;
+		int tempIndex =0;
+		
+		int[] intTemp = new int[end-start];
+		
+		while(i < mid && j< end) {
+			intTemp[tempIndex++] = intArray[i] >= intArray[j] ? intArray[i++] : intArray[j++];
 		}
 		
 		// optimisation sur la copy, copy par bloc... a voir en details
